9027132fa74d729b2c0616039e755b19408349b5
BPMData <bryan.patrick.a.murphy@gmail.com>
2023-06-21 16:40:31 -0400

    Day 22:
    Add the titles of the projects I'll be completing to my Web portfolio.

--- main.py
+++ main.py
@@ -2,15 +2,24 @@
 
 import streamlit as st
 from annotated_text import annotated_text as at
+from streamlit_extras.colored_header import colored_header
+from markdownlit import mdlit
+from streamlit_extras.switch_page_button import switch_page
+from streamlit_card import card
+#lmao
+from streamlit_extras.let_it_rain import rain
+import pandas
 
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+rain("👻")
 
-with col1:
-    st.image("images/cormanowild.jpg", width=600)
+headcol1, headcol2 = st.columns(2)
 
-with col2:
+with headcol1:
+    st.image("images/cormanowild.jpg")
+
+with headcol2:
     st.title("Cormano Wild")
     content = """ In reasonable compliment favourable is connection dispatched in terminated. Do esteem object we called father excuse remove. So dear real on like more it. Laughing for two families addition expenses surprise the. If sincerity he to curiosity arranging. Learn taken terms be as. Scarcely mrs produced too removing new old.
 
@@ -25,3 +34,16 @@ at("Below you can find some of the apps I have built in ",
                    "and in ",
                    ("R", "","#faf"),
                    ". Feel free to contact me!")
+
+import pandas
+data = pandas.read_csv ("data.csv", sep=",")
+
+bodycol1, bodycol2 = st.columns(2)
+
+with bodycol1:
+    for index, row in data[:10].iterrows():
+        st.header(row["title"])
+
+with bodycol2:
+    for index, row in data[10:].iterrows():
+        st.header(row["title"])
