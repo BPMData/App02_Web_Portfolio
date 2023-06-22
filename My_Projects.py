882416a851395b37a25b24ad7c86433dbfe525bd
BPMData <bryan.patrick.a.murphy@gmail.com>
2023-06-21 21:23:06 -0400

    Undo this commit once deployment works...

--- My_Projects.py
+++ My_Projects.py
@@ -18,19 +18,17 @@ rain("💻", falling_speed=-10)
 #
 # with headcol1:
 #     st.image("images/cormanowild.jpg")
+# with headcol2:
 
-headcol2 = st.columns(1)
-
-with headcol2:
-    st.title("Cormano Wild")
-    content = """ In reasonable compliment favourable is connection dispatched in terminated. Do esteem object we called father excuse remove. So dear real on like more it. Laughing for two families addition expenses surprise the. If sincerity he to curiosity arranging. Learn taken terms be as. Scarcely mrs produced too removing new old.
+st.title("Cormano Wild")
+content = """ In reasonable compliment favourable is connection dispatched in terminated. Do esteem object we called father excuse remove. So dear real on like more it. Laughing for two families addition expenses surprise the. If sincerity he to curiosity arranging. Learn taken terms be as. Scarcely mrs produced too removing new old.
 
 Am terminated it excellence invitation projection as. She graceful shy believed distance use nay. Lively is people so basket ladies window expect. Supply as so period it enough income he genius. Themselves acceptance bed sympathize get dissimilar way admiration son. Design for are edward regret met lovers. This are calm case roof and. """
-    st.info(content)
+st.info(content)
 
-    contact_me2 = st.button("Contact me!")
-    if contact_me2:
-        switch_page("Contact Me")
+contact_me2 = st.button("Contact me!")
+if contact_me2:
+    switch_page("Contact Me")
 #
 # greeting = "Below you can find some of the apps I have built in Python and in R. Feel free to contact me!"
 # st.subheader(greeting)
